1ba5269e2be19b2a9840484a1d868111376ffd3d
mshan <mhshan@126.com>
2019-09-20 14:31:50 +0800

    增加国际化反向工程，加密软件的代码读取功能

---
+++ Mshan.Document.WinFormDatabase/FrmUnLanguage.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Mshan.Document.WinFormDatabase
+{
+    partial class FrmUnLanguage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxExtension = new System.Windows.Forms.ComboBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNotice = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnUnExtension = new System.Windows.Forms.Button();
+            this.btnExtension = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLanguage = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxDestExtension = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbxDestExtension);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbxExtension);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNotice);
+            this.groupBox1.Location = new System.Drawing.Point(16, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1018, 422);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "提  示";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "源扩展类型：";
+            // 
+            // cbxExtension
+            // 
+            this.cbxExtension.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxExtension.FormattingEnabled = true;
+            this.cbxExtension.Items.AddRange(new object[] {
+            ".cs",
+            ".txt",
+            ".cpp",
+            ".c",
+            ".java"});
+            this.cbxExtension.Location = new System.Drawing.Point(127, 389);
+            this.cbxExtension.Name = "cbxExtension";
+            this.cbxExtension.Size = new System.Drawing.Size(222, 24);
+            this.cbxExtension.TabIndex = 3;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(82, 344);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(927, 26);
+            this.txtPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "路径：";
+            // 
+            // txtNotice
+            // 
+            this.txtNotice.Location = new System.Drawing.Point(23, 25);
+            this.txtNotice.Multiline = true;
+            this.txtNotice.Name = "txtNotice";
+            this.txtNotice.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNotice.Size = new System.Drawing.Size(986, 306);
+            this.txtNotice.TabIndex = 0;
+            this.txtNotice.WordWrap = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUnExtension);
+            this.groupBox2.Controls.Add(this.btnExtension);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnLanguage);
+            this.groupBox2.Controls.Add(this.btnClose);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Controls.Add(this.btnLoad);
+            this.groupBox2.Location = new System.Drawing.Point(16, 446);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(1009, 76);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "操  作";
+            // 
+            // btnUnExtension
+            // 
+            this.btnUnExtension.Location = new System.Drawing.Point(430, 28);
+            this.btnUnExtension.Name = "btnUnExtension";
+            this.btnUnExtension.Size = new System.Drawing.Size(120, 36);
+            this.btnUnExtension.TabIndex = 6;
+            this.btnUnExtension.Text = "去除扩展名";
+            this.btnUnExtension.UseVisualStyleBackColor = true;
+            this.btnUnExtension.Click += new System.EventHandler(this.btnUnExtension_Click);
+            // 
+            // btnExtension
+            // 
+            this.btnExtension.Location = new System.Drawing.Point(296, 28);
+            this.btnExtension.Name = "btnExtension";
+            this.btnExtension.Size = new System.Drawing.Size(120, 36);
+            this.btnExtension.TabIndex = 5;
+            this.btnExtension.Text = "增加扩展名";
+            this.btnExtension.UseVisualStyleBackColor = true;
+            this.btnExtension.Click += new System.EventHandler(this.btnExtension_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(706, 26);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 36);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "清  除";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLanguage
+            // 
+            this.btnLanguage.Location = new System.Drawing.Point(162, 28);
+            this.btnLanguage.Name = "btnLanguage";
+            this.btnLanguage.Size = new System.Drawing.Size(120, 36);
+            this.btnLanguage.TabIndex = 3;
+            this.btnLanguage.Text = "语言文件";
+            this.btnLanguage.UseVisualStyleBackColor = true;
+            this.btnLanguage.Click += new System.EventHandler(this.btnLanguage_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(846, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(120, 36);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "关  闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(563, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "处  理";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(20, 28);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(120, 36);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "加  载 ";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(411, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "目标扩展类型：";
+            // 
+            // cbxDestExtension
+            // 
+            this.cbxDestExtension.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxDestExtension.FormattingEnabled = true;
+            this.cbxDestExtension.Items.AddRange(new object[] {
+            ".cs",
+            ".txt",
+            ".cpp",
+            ".c",
+            ".java"});
+            this.cbxDestExtension.Location = new System.Drawing.Point(537, 386);
+            this.cbxDestExtension.Name = "cbxDestExtension";
+            this.cbxDestExtension.Size = new System.Drawing.Size(222, 24);
+            this.cbxDestExtension.TabIndex = 6;
+            // 
+            // FrmUnLanguage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 532);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmUnLanguage";
+            this.Text = "FrmUnLanguage";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNotice;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnLanguage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExtension;
+        private System.Windows.Forms.Button btnUnExtension;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxExtension;
+        private System.Windows.Forms.ComboBox cbxDestExtension;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Mshan.Document.WinFormDatabase/FrmUnLanguage.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace Mshan.Document.WinFormDatabase
+{
+    public partial class FrmUnLanguage : Form
+    {
+        public FrmUnLanguage()
+        {
+            InitializeComponent();
+        }
+        string __path = string.Empty;
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browerDialog = new FolderBrowserDialog();
+            if (!string.IsNullOrEmpty(__path))
+                browerDialog.SelectedPath = __path;
+            browerDialog.ShowDialog(this);
+            __path = browerDialog.SelectedPath;
+            if (!string.IsNullOrEmpty(__path) && System.IO.Directory.Exists(__path))
+            {
+                List<string> list = GetFileList(__path);
+                foreach (string s in list)
+                    WriteControl(s);
+            }
+            //MessageBox.Show("加载成功", "提示");
+        }
+        public string[] __exclude = new string[] { "bin", "obj", "Properties" };
+        public List<string> GetFileList(string path)
+        {
+            List<string> list = new List<string>();
+            string[] dir = System.IO.Directory.GetDirectories(path);
+            string[] fileName = System.IO.Directory.GetFiles(path);
+            for (int i = 0; i < dir.Length; i++)
+            {
+                if (ExcludeDir(dir[i]))
+                    continue;
+                list.AddRange(GetFileList(dir[i]));
+            }
+            for (int i = 0; i < fileName.Length; i++)
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(fileName[i], "^.*\\" + SourceExtension + "$"))
+                    list.Add(fileName[i]);
+            }
+            return list;
+        }
+        public bool ExcludeDir(string path)
+        {
+            //if (System.Text.RegularExpressions.Regex.IsMatch(dir[i], string.Format("\\{0}$",__exclude), System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            //        continue;
+            bool pointer = false;
+            foreach (string s in __exclude)
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(path, string.Format(@"\\{0}$", s), System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    pointer = true;
+                    break;
+                }
+            }
+            return pointer;
+        }
+
+        public void WriteControl(string text)
+        {
+            //if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    txtNotice.AppendText(text + "\r\n");
+                    txtNotice.ScrollToCaret();
+                }));
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string[] path =System.Text.RegularExpressions.Regex.Split(txtNotice.Text,"\r\n");
+            foreach (string p in path)
+            {
+             
+                if (System.IO.File.Exists(p))
+                {
+                    System.IO.StreamReader stringReader = new System.IO.StreamReader(p);
+                    string text = stringReader.ReadToEnd();
+                    stringReader.Dispose();
+                    stringReader.Close();
+                    System.Text.RegularExpressions.MatchCollection mathCollection = System.Text.RegularExpressions.Regex.Matches(text, "(\"\"\\s*\\+\\s*)*ECard.LanguageResources.LanguageUse.GetResourceValue\\(ECard.LanguageResources.LanguageUse.ResourcesType.Message\\)\\[\"(\\w*)\"\\].ToString\\(\\)(\\s*\\+\\s*\"\")*");
+                    foreach (System.Text.RegularExpressions.Match match in mathCollection)
+                    {
+                        text = text.Replace(match.Groups[0].Value, "\"" + __dic[match.Groups[2].Value].ToString() + "\"");
+                    }
+                    System.IO.StreamWriter writer = new System.IO.StreamWriter(p);
+                    writer.Write(text);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+            MessageBox.Show("处理完成", "提示");
+
+        }
+        public Dictionary<string, object> __dic = new Dictionary<string, object>();
+        string __fileName = string.Empty;
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLanguage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (!string.IsNullOrEmpty(__fileName))
+                fileDialog.FileName = __fileName;
+            fileDialog.ShowDialog(this);
+            __fileName = fileDialog.FileName;
+            if (!System.IO.File.Exists(__fileName))
+                return;
+            System.Xml.XmlDocument document = new System.Xml.XmlDocument();
+            document.Load(__fileName);
+            System.Xml.XmlNodeList nodeList = document.SelectNodes("resources/textItems/items");
+            __dic = new Dictionary<string, object>();
+            foreach (System.Xml.XmlNode xmlNode in nodeList)
+            {
+                System.Xml.XmlNodeList childList = xmlNode.ChildNodes;
+                Dictionary<string, object> dic = new Dictionary<string, object>();
+                foreach (System.Xml.XmlNode childNode in childList)
+                {
+                    if (!string.IsNullOrEmpty(childNode.InnerText))
+                        dic.Add(childNode.Attributes["key"].Value, childNode.InnerText);
+
+                }
+                __dic.Add(xmlNode.Attributes["key"].Value, __dic);
+            }
+            System.Xml.XmlNodeList messageList = document.SelectNodes("resources/commonSet/MES/item");
+            foreach (System.Xml.XmlNode xmlNode in messageList)
+            {
+                if (!string.IsNullOrEmpty(xmlNode.Attributes["key"].Value))
+                    __dic.Add(xmlNode.Attributes["key"].Value, xmlNode.InnerText);
+            }
+            MessageBox.Show("加载成功", "提示");
+
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNotice.Text = string.Empty;
+        }
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, StringBuilder lParam);
+        [DllImport("User32.DLL")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        /// <summary>
+        /// 获取对话框中子窗口控件的句柄
+        /// </summary>
+        [DllImport("User32.DLL")]
+        public extern static IntPtr GetDlgItem(IntPtr hDlg, int nControlID);
+        [DllImport("user32.dll")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int nMaxCount);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextEX(IntPtr hWnd, StringBuilder text, int nMaxCount);
+        /// <summary>
+        /// 回调函数代理
+        /// </summary>
+        public delegate bool CallBack(IntPtr hwnd, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern int EnumWindows(CallBack lpfn, int lParam);
+        [DllImport("user32.dll")]
+        public static extern int EnumChildWindows(IntPtr hWndParent, CallBack lpfn, int lParam);
+        public const uint WM_SETTEXT = 0x000C;
+        public static int WM_GETTEXT = 0x000D;
+        /// <summary>
+        /// 子窗口回调处理函数
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public static bool ChildWindowProcess(IntPtr hwnd, IntPtr lParam)
+        {
+            Int32 length = GetWindowTextLength(hwnd);
+            StringBuilder text = new StringBuilder(4028);
+            SendMessage(hwnd, WM_GETTEXT, 4028, text);
+            return true;
+        }
+        /// <summary>
+        /// 窗口回调处理函数
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public static bool WindowProcess(IntPtr hwnd, IntPtr lParam)
+        {
+            EnumChildWindows(hwnd, new CallBack(ChildWindowProcess), 0);
+            return true;
+        }
+        private string ChangeExtension(string path)
+        {
+            System.Diagnostics.Process proc;
+            try
+            {
+                // 启动记事本
+                proc = new System.Diagnostics.Process();
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.Arguments = path;
+                proc.StartInfo.FileName = "notepad.exe";
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                proc = null;
+
+            } 
+            if (proc != null)
+            {
+                System.Threading.Thread.Sleep(1000);
+                // 调用 API, 传递数据
+                while (proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    proc.Refresh();
+                }
+                Int32 length = 1024 * 4000;
+                StringBuilder text = new StringBuilder(length);
+                IntPtr vHandle = FindWindowEx(proc.MainWindowHandle, IntPtr.Zero, null, null);
+                // 传递数据给记事本
+                SendMessage(vHandle, WM_GETTEXT, length, text);
+                //txtNotice.AppendText(text.ToString());
+                proc.CloseMainWindow();
+                return text.ToString();
+            }
+            return string.Empty; 
+        }
+        public static void WriterFile(string path, string text)
+        {
+            System.IO.FileStream rw = System.IO.File.Open(path, System.IO.FileMode.Create);
+            byte[] info = new UTF8Encoding(true).GetBytes(text);
+            rw.Write(info, 0, info.Length);
+            rw.Flush();
+            rw.Close();
+        }
+        private void btnExtension_Click(object sender, EventArgs e)
+        {
+            string newPath = txtPath.Text;
+            if (string.IsNullOrEmpty(newPath))
+            {
+                MessageBox.Show(this, "请输入新的路径", "提示");
+                return;
+            }
+            if (!string.IsNullOrEmpty(__path) && System.IO.Directory.Exists(__path))
+            {
+                List<string> list = GetFileList(__path);
+                foreach (string s in list)
+                {
+                    string relativePath = s.Substring(__path.Length);
+                    string newDir = newPath + relativePath.Substring(0, relativePath.LastIndexOf('\\'));
+                    if (!System.IO.Directory.Exists(newDir))
+                    {
+                        System.IO.Directory.CreateDirectory(newDir);
+                    }
+                    string text = ChangeExtension(s);
+                    WriterFile(newPath + relativePath + DestExtension, text);
+                }
+            }
+        }
+        public string SourceExtension 
+        {
+            get{
+                if (string.IsNullOrEmpty(cbxExtension.Text))
+                    return ".cs";
+                return cbxExtension.Text;
+            }
+        }
+        public string DestExtension
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(cbxDestExtension.Text))
+                    return ".txt";
+                return cbxDestExtension.Text;
+            }
+        }
+        private void btnUnExtension_Click(object sender, EventArgs e)
+        {
+            List<string> list = GetFileList(__path);
+            foreach (string s in list)
+            {
+                if (s.EndsWith(SourceExtension, true, System.Globalization.CultureInfo.CurrentCulture))
+                {
+                    string newFile = s.Substring(0, s.Length - SourceExtension.Length);
+                    System.IO.File.Move(s, newFile);
+                }
+            }
+        }
+
+
+
+    }
+}
